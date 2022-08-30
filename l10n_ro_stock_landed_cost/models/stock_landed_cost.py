375e5e4d568a08a24dbba811b317facbddc50abb
Nexterp <teodoralexandru@nexterp.ro>
2022-08-30 11:28:48 +0300

    landed cost, not posible if nothing remained in svl

--- l10n_ro_stock_landed_cost/models/stock_landed_cost.py
+++ l10n_ro_stock_landed_cost/models/stock_landed_cost.py
@@ -13,6 +13,16 @@ class StockLandedCost(models.Model):
 
     def button_validate(self):
         for rec in self:
-            if rec.company_id.l10n_ro_accounting and rec.account_move_id:
-                raise UserError(_(f"For Landed Cost = ({rec.id}, {rec.name}), you already have a Journal Entry. You can NOT revalidate. Create another Landed Cost (the reason is that you'll have old svl with this landed cost) "))
+            if rec.company_id.l10n_ro_accounting:
+                if rec.account_move_id:
+                    raise UserError(_(f"For Landed Cost = ({rec.id}, {rec.name}), you already have a Journal Entry. You can NOT revalidate. Create another Landed Cost (the reason is that you'll have old svl with this landed cost) "))
+                # we must rise error if the stock moves svl have remaining_qty 0
+                # this can be a serious problem at dvi, but also at landed cost
+                # because it will not create all svl's that is supposed to
+                move_lines = rec.picking_ids.move_lines
+                if not move_lines:
+                    raise UserError(_(f"For landed cost=({rec.id},{rec.name}) you do not have stock_move_lines to distribute the cost"))
+                svl_without_remaining_qty = move_lines.stock_valuation_layer_ids.filtered(lambda r: not r.remaining_qty)
+                if svl_without_remaining_qty:
+                    raise UserError(_(f"For landed cost=({rec.id},{rec.name}) you do not svl_without_remaining_qty={svl_without_remaining_qty}"))
         return super().button_validate()
